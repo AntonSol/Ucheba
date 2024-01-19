f83145284eecf57d7d9ab42b5e4b6a1c921f2646
Anton Solukov <solukov.anton@gmail.com>
2024-01-19 14:41:35 +0300

    1

--- test5.js
+++ test5.js
@@ -1,3 +1,13 @@
-let age =prompt('Skolko let?','')
-let accessAllowed=(age>18)? true:false
-alert (accessAllowed)
+const x=+prompt('Число от 0 до 3','')
+switch(x){
+    case 0:
+        alert('это число ноль');break
+    case 1:
+        alert('это один');break    
+    case 2:
+    case 3:
+        alert('это два или три');break 
+    default:
+        alert('вне зоны обработки')    
+
+}
